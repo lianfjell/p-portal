b8cb8f39c1651caa6c61511b7eefb85dbf69cc2c
lianfjell <web@lianfjell.no>
2019-11-21 09:59:48 +0100

    added landingpage for logged in users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#user authentication
+gem 'devise'
 #jQuery
 gem 'jquery-rails'
 #Bootstrap
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191113094639) do
+ActiveRecord::Schema.define(version: 20191113095527) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.string "title"
     t.string "workplace"
     t.string "workcommune"
@@ -23,6 +22,13 @@ ActiveRecord::Schema.define(version: 20191113094639) do
     t.string "method"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
