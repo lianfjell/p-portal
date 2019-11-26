eed870cbbf0b6359596c48be606fe691ed8a3a62
lianfjell <web@lianfjell.no>
2019-11-26 13:33:25 +0100

    added article overview and styling on user-profile-page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
                                         nav_thumb: "30x30#",
                                         post_thumb: "50x50#", 
                                         post_form_thumb: "60x60#", 
-                                        profile_page_image:"150x150#" }, 
+                                        profile_page_image:"130x130#" }, 
                                         default_url: "/:style_missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
